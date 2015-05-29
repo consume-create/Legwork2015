7090062ffaf59fb7f68303d765051d7344347714
DrewDahlman <info@drewdahlman.com>
2015-05-29 11:12:46 -0600

    reverting to serve statics

--- www/routes/index.js
+++ www/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/*', function(req, res, next) {
-	// res.render('index.html');
-	res.json({hello:"world"})
+	res.render('index.html');
+	// res.json({hello:"world"})
 });
 
 module.exports = router;
