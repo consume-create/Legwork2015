4e2b954453ff196ed16ca81aeeb41fe3c8b72264
DrewDahlman <info@drewdahlman.com>
2015-05-29 11:10:00 -0600

    testing json

--- www/routes/index.js
+++ www/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/*', function(req, res, next) {
-	res.render('index.html');
+	// res.render('index.html');
+	res.json({hello:"world"})
 });
 
 module.exports = router;
